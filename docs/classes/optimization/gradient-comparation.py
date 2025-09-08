557eef0ad9dc8b3c24fa6de53f171b22a82d4be6
Humberto Sandmann <hsandmann@ieee.org>
2025-09-08 12:06:18 -0300

    fixed libraries

--- docs/classes/optimization/gradient-comparation.py
+++ docs/classes/optimization/gradient-comparation.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation, PillowWriter
 
 # Define the loss function: f(x, y) = x^2 + y^2 + 0.5
